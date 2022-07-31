a23d2ed187fe243ce262e9190655515d9034e0cc
Mert Ali Kocak <mertalikocakk@gmail.com>
2022-08-01 02:54:12 +0300

    bug fixes

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/goals/";
+const API_URL = process.env.REACT_APP_API + "/api/goals/";
 
 //create new goal
 const createGoal = async (goalData, token) => {
